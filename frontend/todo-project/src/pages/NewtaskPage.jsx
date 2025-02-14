94b7e93fce8be7421b750cb9e721f09c190e0e76
BellTitiporn <139092027+BellTitiporn@users.noreply.github.com>
2025-02-14 11:49:33 +0700

    Create NewtaskPage.jsx

---
+++ frontend/todo-project/src/pages/NewtaskPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function NewtaskPage() {
+    return (
+        <div>
+            <Navbar />
+            <h1 style={{ textAlign: "center" }}>New task</h1>
+        </div>
+    )
+}
+
+export default NewtaskPage
