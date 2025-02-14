6e7878561f934a416fcf31ab0ce38d9fed5ef5d5
BellTitiporn <139092027+BellTitiporn@users.noreply.github.com>
2025-02-14 11:47:41 +0700

    Create CompletedPage.jsx

---
+++ frontend/todo-project/src/pages/CompletedPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function CompletedPage() {
+    return (
+        <div>
+            <Navbar />
+            <h1 style={{ textAlign: "center" }}>Completed</h1>
+        </div>
+    )
+}
+
+export default CompletedPage
