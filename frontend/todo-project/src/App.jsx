5a1dc8d8eb11151c7c0680ac5abf32792da531d1
BellTitiporn <139092027+BellTitiporn@users.noreply.github.com>
2025-02-14 11:46:53 +0700

    Update App.jsx
    add Link to path

--- frontend/todo-project/src/App.jsx
+++ frontend/todo-project/src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import LoginPage from './pages/LoginPage';
+import Router from './routes/Router'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <LoginPage></LoginPage>
+      <Router />
     </div>
   )
 }
